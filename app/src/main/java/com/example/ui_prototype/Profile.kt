0ee05ef891b4f1be2749c59e34633b2fc711cd17
Saaransh Sharma <saaranshsharma28@gmail.com>
2023-11-04 22:26:45 -0400

    Adding edit profile functions

--- app/src/main/java/com/example/ui_prototype/Profile.kt
+++ app/src/main/java/com/example/ui_prototype/Profile.kt
@@ -5,28 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ui_prototype.databinding.ActivityMainBinding
+
+import androidx.navigation.fragment.findNavController
+
 import com.example.ui_prototype.databinding.FragmentProfileBinding
-import com.example.ui_prototype.databinding.ProfileTemplateBinding
+
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 
 
 class Profile : Fragment() {
 
     // Firebase Instances
-    private lateinit var auth: FirebaseAuth;
-    private lateinit var firestore : FirebaseFirestore;
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
 
     // Binding
-    private lateinit var binding: FragmentProfileBinding;
+    private lateinit var binding: FragmentProfileBinding
 
+    private var currentUser: FirebaseUser? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        
+
         // Inflate the layout for this fragment using View Binding
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         val view = binding.root
@@ -36,7 +40,13 @@ class Profile : Fragment() {
         firestore = FirebaseFirestore.getInstance()
 
         // Get current user from FireAuth
-        val currentUser = auth.currentUser
+        currentUser = auth.currentUser
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         currentUser?.let { user ->
             // Use the user's UID to reference the correct document in the 'users' collection
@@ -46,17 +56,28 @@ class Profile : Fragment() {
 
                         // Grab FireStore Fields
                         val username = document.getString("username") ?: "No Username"
-                        val email = document.getString("email") ?: "No Email"
+                        var name = document.getString("firstname") ?: "GeoPic User"
+                        val bio = document.getString("bio") ?: ""
 
                         // Set Profile Fields
-                        binding.profileTemplate.profileName.text = email
-                        binding.profileTemplate.userName.text = username
-                        // Populate other fields
+                        if (name.isEmpty()) {
+                            name = "GeoPic User"
+                        }
+                        binding.profileTemplate.profileName.text = name
+                        binding.profileTemplate.userName.text = "@$username"
+                        binding.profileTemplate.profileDescription.text = bio
+
                     }
                 }
         }
 
-        return view
+        binding.profileTemplate.editProfileButton.setOnClickListener {
+
+            findNavController().navigate(R.id.action_Profile_to_EditProfile)
+
+        }
+
     }
+
 
 }
