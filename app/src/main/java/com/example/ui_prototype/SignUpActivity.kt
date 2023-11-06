97098af08dcf5305e2060f5401f140cabd0068bf
ahmetkca <ahmet.karapinar@ontariotechu.net>
2023-11-05 19:56:11 -0500

    Add camera functionality with capture photo and record video. Show preview of the captured media content. Allow user to retake or use the captured media content to post it to their profile.

--- app/src/main/java/com/example/ui_prototype/SignUpActivity.kt
+++ app/src/main/java/com/example/ui_prototype/SignUpActivity.kt
@@ -20,6 +20,7 @@ class SignUpActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
