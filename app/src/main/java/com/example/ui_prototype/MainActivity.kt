14b85742496077488c691ba27b7c82493cffefe2
Saaransh Sharma <saaranshsharma28@gmail.com>
2023-11-26 18:18:36 -0500

    adding exit button

--- app/src/main/java/com/example/ui_prototype/MainActivity.kt
+++ app/src/main/java/com/example/ui_prototype/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.ui_prototype
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.ImageButton
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -28,35 +30,50 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar) // Replace with your Toolbar ID
         setSupportActionBar(toolbar)
 
+
+        val imageButton = ImageButton(this)
+        imageButton.setImageResource(R.drawable.baseline_logout_24) // Set your icon here
+        imageButton.setBackgroundResource(R.color.blue_main)
+        imageButton.setPadding(0, 0, 50, 0)
+
+        imageButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut() // Sign out from Firebase
+            val signInIntent = Intent(this, SignInActivity::class.java)
+            signInIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(signInIntent)
+            finish() // Close the MainActivity
+            true
+        }
+
+        val params = Toolbar.LayoutParams(
+            Toolbar.LayoutParams.WRAP_CONTENT,
+            Toolbar.LayoutParams.WRAP_CONTENT
+        )
+        params.gravity = Gravity.END
+        toolbar.addView(imageButton, params)
+
         // Setup the navController from NavHostFragment
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
 
         // Define appBarConfiguration with top-level destinations
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.home,
-            R.id.mapsFragment,
-            R.id.camera,
-            R.id.profile
-        ))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.home,
+                R.id.mapsFragment,
+                R.id.camera,
+                R.id.profile
+            )
+        )
 
         // Setup BottomNavigationView with navController
         binding.bottomNavigationView.setupWithNavController(navController)
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_logout -> {
-                    FirebaseAuth.getInstance().signOut() // Sign out from Firebase
-                    val signInIntent = Intent(this, SignInActivity::class.java)
-                    signInIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    startActivity(signInIntent)
-                    finish() // Close the MainActivity
-                    true
-                }
-                else -> {
-                    // Let the NavController handle the navigation for other items
-                    NavigationUI.onNavDestinationSelected(item, navController)
-                }
-            }
+
+            // Let the NavController handle the navigation for other items
+            NavigationUI.onNavDestinationSelected(item, navController)
+
         }
         // Setup the ActionBar with navController and appBarConfiguration
         setupActionBarWithNavController(navController, appBarConfiguration)
